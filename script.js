07afe012e6eb5fa85d1ae9b677f4ca83ef169c80
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-10-16 15:30:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function submitBoyfriend() {
         const content = document.getElementById("content");
         content.innerHTML = `
             <div class="pop-up">
-                <p> ehen🌚🌚 so it's  ${boyfriendName}</p>
+                <p> ehen🌚🌝 so it's  ${boyfriendName}</p>
                 <p>Would you like to move on to the next question?</p>
                 <button class="btn" id="next-question-btn">Ok</button>
             </div>
