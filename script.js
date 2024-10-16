d87fc7cda4fba132b32d44503c78fcafaed517ab
Ayanokojix2306 <ayanokojix2306@gmail.com>
2024-10-16 19:13:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded"), () => {
     document.getElementById("continue-btn").addEventListener("click", startQuiz);
 });
 
